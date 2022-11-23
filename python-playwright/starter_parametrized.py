18a862ef8867365b6f7637bb4ed53e2fc05de045
Pyhajarvi Maaret <maaret.pyhajarvi@vaisala.com>
2022-11-23 20:37:11 +0200

    add three tests into parametrized format

--- python-playwright/starter_parametrized.py
+++ python-playwright/starter_parametrized.py
@@ -11,12 +11,15 @@ def this_is_sample():
 
 @pytest.mark.parametrize('input_text, expect_wordcount, expect_discouraged, expect_violation',
 [
-    ("word", "1", "0", "0")
+    ("word", "1", "0", "0"),
+    ("If programming's the act of teaching a computer to have a conversation with a user, it most useful to first teach the computer how to speak.","26","0","1"),
+    #bug: incorrectly recognized - should be recognized and isn't. 
+    #("you're","2","1","0"),
 ])
 def test_parametrized_test(page: Page, input_text, expect_wordcount, expect_discouraged, expect_violation):
     page.goto(url)
-    page.fill("#inputtext", input_text)
-    page.click("#CheckForEPrimeButton")
+    page.locator("#inputtext").fill(input_text)
+    page.locator("#CheckForEPrimeButton").click()
     expect(page.locator('#eprimeoutput')).to_have_text(input_text)
     expect(page.locator("#wordCount")).to_have_text(str(expect_wordcount))
     expect(page.locator("#discouragedWordCount")).to_have_text(str(expect_discouraged))
