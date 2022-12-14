58ff14d3170a399c1a58a2674a54b3b7f4ffed8a
Pyhajarvi Maaret <maaret.pyhajarvi@vaisala.com>
2022-12-14 22:26:59 +0200

    Enabled starter tests and copy of courseware 2.2.

--- python-playwright/starter_parametrized.py
+++ python-playwright/starter_parametrized.py
@@ -12,17 +12,17 @@ def this_is_sample():
 @pytest.mark.parametrize('input_text, expect_wordcount, expect_discouraged, expect_violation',
 [
     ("word", "1", "0", "0"),
-    #("If programming's the act of teaching a computer to have a conversation with a user, it most useful to first teach the computer how to speak.","26","0","1"),     #bug: programming's is counted as two words when it is shortening for is-verb
-    #("we're", "1", "1", "0") #bug: 're incorrectly recognized - should be recognized and isn't.
-    #("be\nare\nis", "3", "3", "0"), #bug miscalculation
-    #("be \nare \nis", "3", "3", "0"), #bug extra space on grey area
-    #("The whole new world !", "4", "0", "0"), #bug symbols surounded by space(s) are counted as words 
-    #("Martha_@gmail.com", "1", "0", "0"),
-    #("\"be\"", "1", "1", "0"),
-    #(10000*"be" , "1", "0", "0"), #bug doesn fit the grey box
-    #(10000*"be " , "10000", "10000", "0"), #bug: no vertical scrolling (fixed)
-    #("'be'", "1", "1", "0") #bug single quotes makes "be" unrecognizable
-    #(this_is_sample(), "31172", "21", "1")
+    ("If programming's the act of teaching a computer to have a conversation with a user, it most useful to first teach the computer how to speak.","26","0","1"),     #bug: programming's is counted as two words when it is shortening for is-verb
+    ("we're", "1", "1", "0"), #bug: 're incorrectly recognized - should be recognized and isn't.
+    ("be\nare\nis", "3", "3", "0"), #bug miscalculation
+    ("be \nare \nis", "3", "3", "0"), #bug extra space on grey area
+    ("The whole new world !", "4", "0", "0"), #bug symbols surounded by space(s) are counted as words 
+    ("Martha_@gmail.com", "1", "0", "0"),
+    ("\"be\"", "1", "1", "0"),
+    (10000*"be" , "1", "0", "0"), #bug doesn fit the grey box
+    (10000*"be " , "10000", "10000", "0"), #bug: no vertical scrolling (fixed)
+    ("'be'", "1", "1", "0"), #bug single quotes makes "be" unrecognizable
+    (this_is_sample(), "31172", "21", "1")
 ])
 def test_parametrized_test(page: Page, input_text, expect_wordcount, expect_discouraged, expect_violation):
     page.goto(url)
